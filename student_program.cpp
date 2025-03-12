9c91a257dbdfbd165adfcb33b9861b975b8e5392
Kanchitpong RMUTL <kanchitpong_sa67@live.rmutl.ac.th>
2025-03-12 15:19:23 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,24 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void GetMatrix( int **value, int *row, int *col ) ;
-void PrintMatrix( int **value, int row, int col ) ;
+void GetMatrix(int **value, int *row, int *col);
+void PrintMatrix(int *value, int row, int col);
 
 int main() {
-    int *data, m = 3, n = 3 ;
-    data = new int[ m * n ] ;
-    GetMatrix( &data, &m, &n ) ;
-    PrintMatrix( &data, m, n ) ;
-    return 0 ;
+    int *data;
+    int m = 3, n = 3;
+
+    data = new int[m * n];
+    GetMatrix(&data, &m, &n);
+    PrintMatrix(data, m, n);
+
+    delete[] data;
+    return 0;
+}
 
 void GetMatrix(int **value, int *row, int *col) {
     printf("Enter rows and cols: ");
     scanf("%d %d", row, col);
-    
-    if (*value == NULL) {
-        printf("Memory allocation failed!\n");
-        exit(1);
-    }
+
+    *value = new int[*row * *col];
 
     printf("Enter matrix values:\n");
     for (int i = 0; i < (*row) * (*col); i++) {
